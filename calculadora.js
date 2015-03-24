9adb720e90d2f6b8fa72835dcc47cc87d4820795
Gustavo Bressan <guttt.bressan@gmail.com>
2015-03-24 14:04:45 -0300

    merge guttt

--- calculadora.js
+++ calculadora.js
@@ -3,9 +3,14 @@ var operador = process.argv[3]
 var num2 = process.argv[4]
 
 if (operador === '+'){
-	console.log('Resultado é:',num1 + num2);
+	console.log('Resultado é:',Number(num1) + Number(num2));
 } else if (operador === '*'){
-	console.log('Resultado é:',num1 * num2);
+	console.log('Resultado é:',Number(num1) * Number(num2));
 } else if (operador === '-'){
-	console.log('Resultado é:',num1 - num2);
+	console.log('Resultado é:',Number(num1) - Number(num2));
 }
+
+console.log("Restando Node Branch");
+
+
+console.log("Balblalbaab");
