2156885b84b9939dac5ee9c3de2022e5a7e95035
Gustavo Bressan <guttt.bressan@gmail.com>
2015-03-24 12:44:42 -0300

    adicionei as multiplicações

--- calculadora.js
+++ calculadora.js
@@ -2,4 +2,10 @@ var num1 = process.argv[2]
 var operador = process.argv[3]
 var num2 = process.argv[4]
 
-console.log("num1,console,num2")
+if (operador === '+'){
+	console.log('Resultado é:',num1 + num2);
+} else if (operador === '*'){
+	console.log('Resultado é:',num1 * num2);
+} else if (operador === '-'){
+	console.log('Resultado é:',num1 - num2);
+}
